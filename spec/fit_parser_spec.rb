1a5e26f7d80014665c781ea134ec89605c60679a
Dima Mescheryakov <dima.mescheryakov@gmail.com>
2016-10-11 13:41:27 +0200

    downgrade bindata to 2.2.0 because of multithreading issue. Add test to check multithreading parsing.

--- spec/fit_parser_spec.rb
+++ spec/fit_parser_spec.rb
@@ -11,6 +11,20 @@ describe FitParser do
   describe '.load_file' do
     it 'works with threads' do
       threads = []
+      %w(
+        be470628-c34a-4189-aae3-42bef36436ce.fit
+        fc84b277-68af-4d63-ac8d-fb8e162ab2a2.fit
+        ).each do |file|
+        threads << Thread.new(file) do |el|
+          data = FitParser.load_file("spec/support/examples/file/#{el}")
+          expect(data.records).to_not be_nil
+        end
+      end
+      threads.each { |thread| thread.join }
+    end
+
+    it 'works with threads' do
+      threads = []
       %w(3863374146 3110334490).each do |file|
         threads << Thread.new(file) do |el|
           data = FitParser.load_file("spec/support/examples/file/#{el}")
@@ -19,5 +33,11 @@ describe FitParser do
       end
       threads.each { |thread| thread.join }
     end
+  end
+
+  it 'works without threads' do
+    path = 'spec/support/examples/file/fc84b277-68af-4d63-ac8d-fb8e162ab2a2.fit'
+    data = FitParser.load_file(path)
+    expect(data.records).to_not be_nil
   end
 end
